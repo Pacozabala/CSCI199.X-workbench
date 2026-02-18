b4a20b77242266d8892ef555a332340f444f5a76
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-02-18 21:55:43 +0800

    fix: train_roberta imports

--- scripts/train_roberta.py
+++ scripts/train_roberta.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from torch.utils.data import Dataset, DataLoader
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
-from transformers import AdamW
+from torch.optim import AdamW
 from sklearn.metrics import f1_score
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")

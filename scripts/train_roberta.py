2db243414b7bfd150e78a79aa103da3b910b34e5
Paco Antonio Zabala <paco.zabala@student.ateneo.edu>
2026-02-18 19:14:40 +0800

    chore:changed filename of dataset prep module

--- scripts/train_roberta.py
+++ scripts/train_roberta.py
@@ -1,6 +1,10 @@
 import torch
-import torch.nn as nn
+import pandas as pd
+import numpy as np
+
 from torch.utils.data import Dataset, DataLoader
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from transformers import AdamW
 from sklearn.metrics import f1_score
-import pandas as pd
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
